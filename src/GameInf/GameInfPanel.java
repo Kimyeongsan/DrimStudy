18e4f80b9226d2f94906de84a158f9c665e3be1c
kimyeongsan <apua34@gmail.com>
2021-08-05 15:58:14 +0900

    실시간 경기정보 Design 완성

--- src/GameInf/GameInfPanel.java
+++ src/GameInf/GameInfPanel.java
@@ -3,22 +3,29 @@ package GameInf;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class GameInfPanel extends JPanel {
+	String event, news;
 	
 	public GameInfPanel(JFrame frame) {
 		super();
 		PanelInit(frame);
-		OutBox();
+		InputBox();
+		OutputBox();
 	}
 
 	private void PanelInit(JFrame frame) {
@@ -30,7 +37,7 @@ public class GameInfPanel extends JPanel {
 		JLabel tilteLabel = new JLabel("메달 순위");
 		tilteLabel.setBounds(45, 35, 450, 40);
 
-		tilteLabel.setFont(new Font("맑은 고딕", Font.BOLD, 25)); // Font
+		tilteLabel.setFont(new Font("맑은 고딕", Font.BOLD, 20)); // Font
 
 		tilteLabel.setForeground(Color.white); // Color
 		tilteLabel.setBackground(Color.black);
@@ -40,54 +47,76 @@ public class GameInfPanel extends JPanel {
 
 		this.add(tilteLabel);
 		
+	}
+	
+	private void InputBox() {
+		// 경기 선택 DropBox
+		String eventList[] = { "축구", "야구", "배구" };
+
+		JComboBox eventBox = new JComboBox<Object>(eventList);
+		eventBox.setBounds(550, 35, 350, 40); 
+
+		this.add(eventBox);
+				
+		// 경기 선택 버튼
+		JButton gameBtn = new JButton("경기 선택");
+		gameBtn.setBounds(920, 35, 130, 40); 
+
+		gameBtn.setFont(new Font("맑은 고딕", Font.BOLD, 20)); // Font
+
+		gameBtn.setForeground(Color.white); // Color
+		gameBtn.setBackground(Color.black);
+		gameBtn.setOpaque(true);
+
+		gameBtn.setHorizontalAlignment(JLabel.CENTER); // Position
+
+		this.add(gameBtn);
 		
-		// Medal Title
-		JLabel tilteLabel1 = new JLabel("DropBox");
-		tilteLabel1.setBounds(550, 35, 500, 40); 
-
-		tilteLabel1.setFont(new Font("맑은 고딕", Font.BOLD, 25)); // Font
-
-		tilteLabel1.setForeground(Color.white); // Color
-		tilteLabel1.setBackground(Color.black);
-		tilteLabel1.setOpaque(true);
-
-		tilteLabel1.setHorizontalAlignment(JLabel.CENTER); // Position
-
-		this.add(tilteLabel1);
+		// 적용버튼 리스너
+		gameBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				event = (String) eventBox.getSelectedItem();
+				
+				System.out.println(event);
+			}
+		});
 		
+		// 이름 입력
+		JTextField newsInput = new JTextField("ex)김연경 스메쉬");
+		newsInput.setBounds(550, 315, 350, 40);
+
+		this.add(newsInput);
 		
-		// Medal Title
-		JLabel tilteLabel2 = new JLabel("하이라이트 검색창");
-		tilteLabel2.setBounds(550, 315, 500, 40); 
+		// 뉴스 검색 버튼
+		JButton newsBtn = new JButton("검색");
+		newsBtn.setBounds(920, 315, 130, 40); 
 
-		tilteLabel2.setFont(new Font("맑은 고딕", Font.BOLD, 25)); // Font
+		newsBtn.setFont(new Font("맑은 고딕", Font.BOLD, 20)); // Font
 
-		tilteLabel2.setForeground(Color.white); // Color
-		tilteLabel2.setBackground(Color.black);
-		tilteLabel2.setOpaque(true);
+		newsBtn.setForeground(Color.white); // Color
+		newsBtn.setBackground(Color.black);
+		newsBtn.setOpaque(true);
 
-		tilteLabel2.setHorizontalAlignment(JLabel.CENTER); // Position
+		newsBtn.setHorizontalAlignment(JLabel.CENTER); // Position
 
-		this.add(tilteLabel2);
+		this.add(newsBtn);
+		
+		// 적용버튼 리스너
+		newsBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				news = (String) newsInput.getText();
+
+				// 임시 출력
+				System.out.println(news);
+			}
+		});
+		
 	}
 
-	private void OutBox() {
-		// Medal Picture
-//		JLabel MedalImage = new JLabel("사진 예정1");
-//		MedalImage.setBounds(45, 95, 450, 480);
-//
-//		MedalImage.setFont(new Font("맑은 고딕", Font.BOLD, 35)); // Font
-//
-//		MedalImage.setForeground(Color.black); // Color
-//		MedalImage.setBackground(new Color(166, 166, 166));
-//		MedalImage.setOpaque(true);
-//
-//		MedalImage.setHorizontalAlignment(JLabel.CENTER); // Position
-//
-//		this.add(MedalImage);
-//		
-		
-		//picture (선수 사진)
+	private void OutputBox() {
+		// Medal Picture  <- 추후 사진 말고 Parsing Modeling 예정 
 		ImageIcon medalIcon = new ImageIcon("./img/game_test1.PNG");
 		
 		Image medalImg = medalIcon.getImage();
@@ -101,7 +130,7 @@ public class GameInfPanel extends JPanel {
 		add(MedalImage);;
 		
 		
-		// Medal Picture
+		// Medal Picture  <- 추후 사진 말고 Parsing Modeling 예정 
 		JLabel planImage = new JLabel("사진 예정2");
 		planImage.setBounds(550, 95, 500, 200);
 
@@ -116,7 +145,7 @@ public class GameInfPanel extends JPanel {
 		this.add(planImage);
 
 		
-		// Medal Picture
+		// Medal Picture <- 추후 사진 말고 Parsing Modeling 예정 
 		JLabel newsImage = new JLabel("사진 예정3");
 		newsImage.setBounds(550, 375, 500, 200);
 
