6efc6b4e60f067af996c9f6fece966b4679ec6e3
ParkSuJeong74 <evergreen9158@naver.com>
2021-08-06 17:20:21 +0900

    mypage test

--- src/Main/MainFrame.java
+++ src/Main/MainFrame.java
@@ -600,7 +600,7 @@ public class MainFrame {
 	             //login과 main게시판 버튼, mypage 버튼 표시
 	             loginbtn.setVisible(true);
 	             logOutbtn.setVisible(true);
-	             writebtn.setVisible(false);
+	             writebtn.setVisible(true);
 	             c_writebtn.setVisible(false);
 	             myPage_Btn.setVisible(false);
 	             regQuitbtn.setVisible(false);
@@ -656,7 +656,7 @@ public class MainFrame {
              //login과 main게시판 버튼, mypage 버튼 표시
              loginbtn.setVisible(true);
              logOutbtn.setVisible(true);
-             writebtn.setVisible(false);
+             writebtn.setVisible(true);
              c_writebtn.setVisible(false);
              myPage_Btn.setVisible(true);
              regQuitbtn.setVisible(false);
@@ -711,7 +711,7 @@ public class MainFrame {
              //login과 main게시판 버튼, mypage 버튼 표시
              loginbtn.setVisible(true);
              logOutbtn.setVisible(true);
-             writebtn.setVisible(false);
+             writebtn.setVisible(true);
              c_writebtn.setVisible(false);
              myPage_Btn.setVisible(true);
              regQuitbtn.setVisible(false);
